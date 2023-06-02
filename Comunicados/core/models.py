17c0e449eb6f47a7683fc742b53d4112aa49cc46
javiibm <javiera.jibm@gmail.com>
2023-06-02 19:15:20 -0400

    F KEY

--- Comunicados/core/models.py
+++ Comunicados/core/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Comunicado(models.Model):
-    titulo = models.TextField(max_length=100)
-    detalle = models.TextField(max_length=100)
-    #nivel =
-    #categoria
-    fecha_envio = models.DateTimeField()
-    fecha_ultima_modificacion = models.DateTimeField()
-    #publicado_por = 
 
 class Categoria(models.Model):
     nombre = models.TextField(max_length=100)
     descripcion = models.TimeField(max_length=1000)
+
+class Comunicado(models.Model):
+    titulo = models.TextField(max_length=100)
+    detalle = models.TextField(max_length=100)
+    #nivel =
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fecha_envio = models.DateTimeField()
+    fecha_ultima_modificacion = models.DateTimeField()
+    #publicado_por = 
